d27f3baf210b7ccfff243bb4d4efb0e15ca2369a
NelsonMALGAS <zmalgas69@gmail.com>
2023-06-02 10:24:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// script.js
+
+// Get the projects list element
+const projectsList = document.getElementById('projects-list');
+
+// Add event listener to the projects list
+projectsList.addEventListener('click', (event) => {
+  const clickedProject = event.target;
+  console.log('Clicked project:', clickedProject.textContent);
+});
+
+//let aboutMe = document.querySelector('.about-me').innerHTML
+let aboutLink = document.querySelector('.ab')
+let section = document.querySelector('.blank')
+
+aboutLink.addEventListener('click' ,() =>{
+ 
+  section.style.display = 'block'
+})
+
+const closeAboutOverlayBtn = document.querySelector('.blank-btn')
+closeAboutOverlayBtn.addEventListener('click' ,() =>{
+ 
+  section.style.display = ''
+})
+
+const displayProjects = document.querySelector('.projects-display ')
+const displayLink = document.querySelector('.cd')
+
+displayLink.addEventListener('click' ,(e)=>{
+  e.stopPropagation()
+  displayProjects.style.display = 'block'
+})
+
+
+const projectsCloseButton = document.querySelector('.project')
+projectsCloseButton.addEventListener('click',()=>{
+  displayProjects.style.display = ''
+})
+
+const helpBtn = document.querySelector('.help-btn');
+    const guide = document.querySelector('.guide');
+
+    helpBtn.addEventListener('click', (e) => {
+      guide.style.display = 'block'
+    });
+    const helpBtnClose = document.querySelector('.help-btn-close');
+  
+    helpBtnClose.addEventListener('click', () => {
+      guide.style.display = ''
+    });
+
+
+  
+    const contacts = document.querySelector('.ef');
+    const contactsOverlay = document.querySelector('.icons');
+    let text = document.querySelector('.text');
+
+  contacts.addEventListener('click', () => {
+  contactsOverlay.style.display = 'flex';
+  
+  const pieceOfText = '<p class="text">' + 'My Social Media Platforms & Email Address' + '</p>';
+
+  text.innerHTML = pieceOfText;
+});
+
+    const html = document.querySelector('body')
+    html.addEventListener('keydown' ,(event)=>{
+      if (event.keyCode === 32){
+      contactsOverlay.style.display = ''
+      
+  const pieceOfText = '<p class="text"></p>';
+
+  text.innerHTML = pieceOfText;
+      
+    }
+})
+    
